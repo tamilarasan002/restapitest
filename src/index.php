8a900c458ec30eda1a2b7bc32f7a363718848539
Tamilarasan002 <134201479+tamilarasan002@users.noreply.github.com>
2024-08-20 11:27:53 +0530

    Create index.php

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP PDF App</title>
+</head>
+<body>
+    <h1>Welcome to the PHP PDF App</h1>
+    <a href="fetch_pdfs.php">Fetch PDFs</a>
+</body>
+</html>
