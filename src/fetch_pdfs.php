63a53b9bd4d022b998b6a10f969cf3d067936f91
Tamilarasan002 <134201479+tamilarasan002@users.noreply.github.com>
2024-08-20 12:12:38 +0530

    Update fetch_pdfs.php

--- src/fetch_pdfs.php
+++ src/fetch_pdfs.php
@@ -4,15 +4,12 @@ require 'vendor/autoload.php'; // For Azure SDK
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 
-// Azure Blob Storage connection string
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=storingfiles;AccountKey=FVDiDa/6VUYjGmTOql9+kw/oukKQFrlzx7MPQ9aSUgq4+n0THl+NAiMETW/FvK7KX7iA82paZ5Cr+ASt4qor8A==;EndpointSuffix=core.windows.net"
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=storingfiles;AccountKey=FVDiDa/6VUYjGmTOql9+kw/oukKQFrlzx7MPQ9aSUgq4+n0THl+NAiMETW/FvK7KX7iA82paZ5Cr+ASt4qor8A==;EndpointSuffix=core.windows.net";
 
-// Create blob client
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
 try {
-    // List blobs in the container
-    $containerName = "pdf-container";
+    $containerName = "files-storing";
     $listBlobs = $blobClient->listBlobs($containerName);
     $blobs = $listBlobs->getBlobs();
 
@@ -20,7 +17,7 @@ try {
     echo "<select name='pdf_file'>";
 
     foreach ($blobs as $blob) {
-        $blobName = $blob->getName();
+        $blobName = htmlspecialchars($blob->getName(), ENT_QUOTES, 'UTF-8');
         echo "<option value='$blobName'>$blobName</option>";
     }
 
@@ -28,6 +25,6 @@ try {
     echo "<input type='submit' value='Select PDF'>";
     echo "</form>";
 } catch(ServiceException $e) {
-    echo "Error: " . $e->getMessage();
+    echo "Error: " . htmlspecialchars($e->getMessage(), ENT_QUOTES, 'UTF-8');
 }
 ?>
